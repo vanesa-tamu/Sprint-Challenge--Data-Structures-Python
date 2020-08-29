17e78ec13c05ecfd1fd57ca76215000d3b7b8459
vanesa-tamu <vahe5495@tamu.edu>
2020-08-28 19:40:26 -0500

    reverse complete with test

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -3,6 +3,9 @@ class Node:
         self.value = value
         self.next_node = next_node
 
+    def __str__(self):
+        return f'value: {self.value}'
+
     def get_value(self):
         return self.value
 
@@ -12,9 +15,13 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def __str__(self):
+        return f'H: {self.head}'
 
     def add_to_head(self, value):
         node = Node(value)
@@ -39,4 +46,20 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        print(f'self now: {self}')
+        if node is not None:
+            while node.get_next() is not None:
+                self.add_to_head(node.next_node.value)
+                self.reverse_list(node.next_node, None)
+                return self.head.value
+            return node.value
+
+l = LinkedList()
+l.add_to_head(1)
+l.add_to_head(2)
+l.add_to_head(3)
+l.add_to_head(4)
+l.add_to_head(5)
+print(l.head.value)  # should be 5
+l.reverse_list(l.head, None)
+print(l.head.value)  # should be 1
